9668edcfe31cbb8bb9c6db4bb77dc24593316fee
skaur402 <skaur402@my.centennialcollege.ca>
2026-03-01 01:03:27 +0000

    Update MainApp.java

--- src/main/java/comp367/MainApp.java
+++ src/main/java/comp367/MainApp.java
@@ -2,6 +2,20 @@ package comp367;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to COMP367");
+
+        // Morning simulation
+        simulateGreeting(9);   // 9 AM
+        System.out.println("------------------------");
+
+        // Afternoon simulation
+        simulateGreeting(15);  // 3 PM
+    }
+
+    public static void simulateGreeting(int hour) {
+        if (hour < 12) {
+            System.out.println("Good morning, Samreet, Welcome to COMP367");
+        } else {
+            System.out.println("Good afternoon, Samreet, Welcome to COMP367");
+        }
     }
 }
